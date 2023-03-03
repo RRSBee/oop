0535b5de19d48fd721552f7c3bcb2c2ffa53fefd
Raveena <raveena.boedhram@ru.nl>
2023-03-03 19:26:37 +0100

    Idk what I did tbh

--- OOP4/assignment-quiz-start/app/src/quiz/Question.java
+++ OOP4/assignment-quiz-start/app/src/quiz/Question.java
@@ -1,10 +1,13 @@
+// Made by Raveena Boedhram s1074078 
+// Quint van Oorschot s1098812
+
 package quiz;
 
-public abstract class Questions { //BASE CLASS
+public abstract class Question { //BASE CLASS
 
     private int score;
 
-    public Questions (int score) {
+    public Question (int score) {
         setScore(score);
     }
 
@@ -33,13 +36,13 @@ public abstract class Questions { //BASE CLASS
     /**
      * Each question has a score. The score is a number between 1 and 5.
      * If the score is specified with an incorrect value, the question should have a score of 3.
-     * @param score of the question
+     * @param val value of the score of the question
      */
-    public void setScore(int score) {
-        if (score < 1 || score > 5) {
+    public void setScore(int val) {
+        if (val < 1 || val > 5) {
             this.score = 3;
         } else {
-            this.score = score;
+            this.score = val;
         }
     }
 
